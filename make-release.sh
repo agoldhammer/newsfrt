a7aee0b7e8608b802e32adbd175b7018c3cbbea5
Art Goldhammer <art.goldhammer@gmail.com>
2018-11-15 10:47:18 -0500

    updated make-release

--- make-release.sh
+++ make-release.sh
@@ -1,5 +1,9 @@
+echo "Cleaning old compile artifacts"
 lein clean
+echo "Compiling with min profile"
 lein cljsbuild once min
+echo "Saving old app.js as app.js.old in nooze/app/static"
 cp ~/Prog/nooze/app/static/app.js ~/Prog/nooze/app/static/app.js.old
+echo "Copying newly compiled app2.js to nooze/app/static/app.js"
 cp resources/public/js/compiled/app2.js ~/Prog/nooze/app/static/app.js
 
